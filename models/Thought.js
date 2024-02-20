89c4c08b78ea25883d7d37465f93c274316fb3d1
Jarede712 <jaredeic@yahoo.com>
2024-02-20 11:25:29 -0800

    started working on Thought model, added most of the schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,33 @@
-const { Schema, Types } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const thoughtSchema = new Schema();
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
