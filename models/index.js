24106fd8cb3fbb05fc79ff6d692fe32ed916aa07
Jarede712 <jaredeic@yahoo.com>
2024-02-20 10:59:01 -0800

    added models folder with necessary files and compiled into an index.js file

---
+++ models/index.js
@@ -0,0 +1,6 @@
+const Email = require('./Email');
+const Friends = require('./Friends');
+const Thoughts = require('./Thoughts');
+const Username = require('./Friends');
+
+module.exports = { Email, Friends, Thoughts, Username };
